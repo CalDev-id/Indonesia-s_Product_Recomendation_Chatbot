a19148f759483799b823d8923bb45cd88e32b629
Heical Chandra <haical31@gmail.com>
2025-03-10 10:53:03 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import argparse
+from RAG.rag import Rag
+import os
+
+def main():
+
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description="Search for product items.")
+    parser.add_argument('query', type=str, help='The query to search for product items')
+    args = parser.parse_args()
+
+    # Initialize RAG
+    rag = Rag()
+
+    # Perform search using the query from arguments
+    result = rag.rag_search(args.query)
+    
+    # Print results
+    print("Best Match Resource:", result['best_match'])
+    print("LLM Response:", result['llm_response'])
+
+if __name__ == "__main__":
+    main()
